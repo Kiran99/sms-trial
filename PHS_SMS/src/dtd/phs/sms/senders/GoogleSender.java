b552794654a3ded144b98d9a354d0164d301628e
sphamhung <sphamhung@gmail.com>
2011-12-07 08:12:06 +0000

    - Added: Google XMPP sender

--- PHS_SMS/src/dtd/phs/sms/senders/GoogleSender.java
+++ PHS_SMS/src/dtd/phs/sms/senders/GoogleSender.java
@@ -1,16 +1,24 @@
 package dtd.phs.sms.senders;
 
+import android.content.Context;
+import android.content.Intent;
 import dtd.phs.sms.data.entities.MessageItem;
 
 public class GoogleSender implements ISMSSender {
 
-	public GoogleSender(ISMS_SendListener listener) {
+	private ISMS_SendListener listener;
+	private Context context;
+
+	public GoogleSender(ISMS_SendListener listener, Context context) {
+		this.listener = listener;
+		this.context = context;
 	}
 
 	@Override
 	public void send(MessageItem message) {
-		// TODO Auto-generated method stub
-		
+		Intent xmppServiceIntent = new Intent(context,XMPPService.class);
+		XMPPService.messageToSend = message;		
+		context.startService(xmppServiceIntent);
 	}
 
 }
